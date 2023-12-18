fa054107dccd31b8f79b87d6b5260fd99609c41c
moka <halo.mokanh@gmail.com>
2023-12-18 17:10:16 +0700

    ✨ feat(LivePreview.vue): add LivePreview component to provide a container for live preview content

---
+++ .vitepress/theme/components/LivePreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="live-preview">
+    <div class="switcher">
+      <div class="switcher-item">
+        
+      </div>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+
+<style>
+.live-preview {
+  background: grey;
+  padding: 1rem;
+  border-radius: 4px;
+}
+</style>
