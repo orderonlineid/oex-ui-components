b4e1313fddcadfa0d7cd3fab9749e13c499674a3
moka <halo.mokanh@gmail.com>
2023-12-19 15:41:10 +0700

    🔧 fix(LivePreview.vue): fix switcher-item class binding to correctly apply active class based on activeTab value ✨ feat(LivePreview.vue): add support for previewing component in an iframe and switching between preview and code tabs

--- .vitepress/theme/components/LivePreview.vue
+++ .vitepress/theme/components/LivePreview.vue
@@ -1,19 +1,83 @@
 <template>
   <div class="live-preview">
     <div class="switcher">
-      <div class="switcher-item">
-        
+      <div :class="`switcher-item ${activeTab === 'preview' ? 'active' : ''}`" @click="activeTab = 'preview'">
+        Preview
+      </div>
+      <div :class="`switcher-item ${activeTab === 'code' ? 'active' : ''}`" @click="activeTab = 'code'">
+        Code
       </div>
     </div>
-    <slot></slot>
+    <template v-if="activeTab === 'preview'">
+      <iframe
+        :src="source"
+        class="component-preview" :height="300"></iframe>
+    </template>
+
+    <div v-if="activeTab === 'code'">
+      <slot></slot>
+
+    </div>
   </div>
 </template>
 
 
+<script setup>
+import { ref, computed } from 'vue'
+
+
+const props = defineProps({
+  storybookId: String
+})
+
+const source = computed(() => {
+  return `https://64140cf50b8d988c175493a4-ogagtsocbe.chromatic.com/iframe.html?args=&id=${props.storybookId}&viewMode=story`;
+});
+
+const activeTab = ref('preview');
+
+
+
+</script>
+
 <style>
 .live-preview {
-  background: grey;
+  background: var(--vp-c-text-2);
   padding: 1rem;
-  border-radius: 4px;
+  border-radius: 8px;
+}
+
+.switcher {
+  display: flex;
+  background-color: #cfcfcf;
+  align-items: center;
+  border-radius: 8px;
+  width: fit-content;
+  padding: 5px;
+  font-weight: bold;
+  font-size: 12px;
+  margin-bottom: 1.5rem;
+}
+
+.switcher-item {
+  color: #505050;
+  padding: 5px 8px;
+  border-radius: 0.25rem;
+  min-width: 70px;
+  text-align: center;
+  cursor: pointer;
+  display: block;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+}
+
+.switcher-item.active {
+  color: #02B1C3;
+  background-color: white;
+}
+
+.component-preview {
+  background: white;
+  border-radius: 8px;
+  min-width: 100%;
 }
 </style>
